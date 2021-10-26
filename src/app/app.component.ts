e8eea857872015c5580e6bfa95ddebb16fb0e314
Hon0r99 <pose1don.dance@gmail.com>
2021-10-26 13:56:54 +0300

    feet(core): build drag&drop

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,48 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import {CdkDragDrop, moveItemInArray, copyArrayItem,transferArrayItem} from '@angular/cdk/drag-drop';
+
+interface FormItems {
+  type?:string,
+  inputType?:string,
+  placeholder?:string,
+  label?:string,
+  text?:string,
+  options?:Array<string>,
+}
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
-  title = 'form-builder';
+export class AppComponent{
+
+  currentTarget!:string;
+
+  formItems:FormItems[] = [
+    { type: 'input', placeholder: 'placeholder', label: 'Text input'},
+    { type: 'textarea', placeholder: 'placeholder textarea text', label: 'Textarea'},
+    { type: 'button', text: 'Submit', label:'Button'},
+    { type: 'checkbox', text: 'Cheked',label:'Checkbox'},
+    { type:'select', text: 'Text', label:'Select', options: ['val1', 'val2']}
+  ];
+
+  droppedItems:FormItems[] = [];
+
+  drop(event: any){
+    console.log(event)
+    if (event.previousContainer === event.container) {
+      moveItemInArray(event.container.data, event.previousIndex, event.currentIndex);
+    } else {
+      copyArrayItem(event.previousContainer.data,
+                        event.container.data,
+                        event.previousIndex,
+                        event.currentIndex);
+    }
+  }
+
+  select(event:Event){
+    this.currentTarget = (event.target as HTMLInputElement).value
+  }
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,33 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+import { MatExpansionModule } from '@angular/material/expansion';
+import { MatButtonModule } from '@angular/material/button';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { DragDropModule } from '@angular/cdk/drag-drop'
+import {MatCheckboxModule} from '@angular/material/checkbox';
+import { MatIconModule } from '@angular/material/icon';
+import { MatSelectModule } from '@angular/material/select';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+
+    MatExpansionModule,
+    MatButtonModule,
+    MatFormFieldModule,
+    MatInputModule,
+    DragDropModule,
+    MatCheckboxModule,
+    MatIconModule,
+    MatSelectModule
   ],
   providers: [],
   bootstrap: [AppComponent]
